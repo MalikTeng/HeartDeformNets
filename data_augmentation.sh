4eb051fffc45c5d53439bb411bd2e8e47cca392e
MalikTeng <37409509+MalikTeng@users.noreply.github.com>
2025-04-04 14:12:39 +0100

    Included train/valid function mode in data_augmentation.sh

--- data_augmentation.sh
+++ data_augmentation.sh
@@ -11,10 +11,18 @@ SEGDIR="${IMGDIR%/}_seg"
 # Automatically set output directory
 OUTDIR=$(dirname $(dirname "$IMGDIR"))/nii_augmented
 
+# Determine mode based on directory name
+if [[ "$IMGDIR" == *"_train"* ]]; then
+    MODE="train"
+else
+    MODE="val"
+fi
+
 # Print the directories for user confirmation
 echo "Image directory: $IMGDIR"
 echo "Segmentation directory: $SEGDIR"
 echo "Output directory: $OUTDIR"
+echo "Mode: $MODE"
 
 # Run the Python script with user-provided and derived variables
 mpirun -n 4 python data/data_augmentation.py \
@@ -22,5 +30,5 @@ mpirun -n 4 python data/data_augmentation.py \
     --seg_dir "$SEGDIR" \
     --out_dir "$OUTDIR" \
     --modality "$MODAL" \
-    --mode train \
+    --mode "$MODE" \
     --num "$NUM"
